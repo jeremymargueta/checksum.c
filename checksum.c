f81ef43d4b0de03986112f65dcad5dd4d20507b2
jeremymargueta <45952986+jeremymargueta@users.noreply.github.com>
2021-02-16 22:22:24 -0800

    Update checksum.c

--- checksum.c
+++ checksum.c
@@ -50,9 +50,9 @@ int main (int argc, char * argv[], char ** envp) {
 
 
   quotient = sum / (max_int+1);
-  fprintf(quotient);
+  fprintf(stdout, "Num", quotient);
   remainder = sum % (max_int+1);
-  fprintf(, remainder);
+  fprintf(remainder);
   sum = quotient + remainder;
   fprintf("%d", sum); 
 
